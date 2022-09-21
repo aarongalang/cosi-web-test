aa5095afade19012ea752531ea1d7b1aa5c5b6d9
aarongalang <aarongalang@microsoft.com>
2022-09-21 15:55:13 -0400

    changing port to 8080

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ ADD . /go/src/github.com/aarongalang/cosi-web-test
 
 RUN go install github.com/aarongalang/cosi-web-test@latest
 
-ENTRYPOINT /go/bin/golang-docker
+EXPOSE 8080
 
-EXPOSE 2379
+ENTRYPOINT ["/cosi-web-test"]
--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":2379", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 		return
