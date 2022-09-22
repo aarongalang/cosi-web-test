f678037e3b469a68d6e5804ddc8a66d090a9a384
aarongalang <aarongalang@microsoft.com>
2022-09-22 17:41:02 -0400

    changing to file download

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/aarongalang/cosi-web-test
 
 go 1.18
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.1.3 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v0.4.1 // indirect
+	golang.org/x/net v0.0.0-20220921203646-d300de134e69 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Cosi test web app")
+	containerClient, err := azblob.NewContainerClientWithNoCredential("https://agcosiacc.blob.core.windows.net/cosi-driver-test-concreate29e4e98c-8982-482f-b4ea-e8c34476cadf?sp=r&st=2022-09-22T21:35:46Z&se=2022-09-30T05:35:46Z&sv=2021-06-08&sr=c&sig=g0szIhf2yI9WVXea%2BOdgqToXljh7t8Kwe4FOoDZIdaw%3D", nil)
+	if err != nil {
+		fmt.Printf("containerClient: %s", err.Error())
+		return
+	}
+
+	blobClient, err := containerClient.NewBlobClient("LoremIpsum.txt")
+	if err != nil {
+		fmt.Printf("blobClient: %s", err.Error())
+		return
+	}
+
+	file, err := os.Create("data.txt")
+	if err != nil {
+		fmt.Printf("create file: %s", err.Error())
+		return
+	}
+	defer file.Close()
+	defer os.Remove("data.txt")
+
+	err = blobClient.DownloadToFile(context.TODO(), 0, 0, file, azblob.DownloadOptions{})
+	if err != nil {
+		fmt.Printf("download data: %s", err.Error())
+		return
+	}
+
+	data, err := os.ReadFile(file.Name())
+	if err != nil {
+		fmt.Printf("read data: %s", err.Error())
+		return
+	}
+
+	fmt.Fprint(w, "Cosi test web app: "+string(data))
 }
 
 func main() {
